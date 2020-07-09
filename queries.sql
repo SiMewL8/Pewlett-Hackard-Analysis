fc7c8a3e13d4cf2ee4b8e61a8f6e9164f98fb1a1
SiMewL8 <64818157+SiMewL8@users.noreply.github.com>
2020-07-09 01:05:04 -0700

    added queries.sql and retire data

---
+++ queries.sql
@@ -0,0 +1,32 @@
+
+-- practice narrowing down employees
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+
+-- Retirement eligibility born in 1952-55 from hire date in 1985-88
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- counting the number of employees retiring using count
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- saving a list of retiring employees using into
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- making sure retirement info is added onto the list of tables
+SELECT * FROM retirement_info;
